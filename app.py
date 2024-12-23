20fddd952b4616700ba74ff645cd6e232af93e2d
Alex Rohrbach <alex.rohrbach@berkeley.edu>
2024-12-23 09:56:45 -0500

    Added Netflix to company list

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    companies = ["Servicetitan, Inc.", "Meta Platforms, Inc.", "Alphabet, Inc."]
+    companies = ["Servicetitan, Inc.", "Meta Platforms, Inc.", "Alphabet, Inc.", "Netflix, Inc."]
     return render_template('index.html', companies=companies)
 
 if __name__ == "__main__":
